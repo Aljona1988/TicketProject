324ca624a65299eefc62d1a52c60c2f71373f62c
Elena Fedkina <zhrulick@maiil.ru>
2022-10-23 23:45:31 +0300

    manager and repo

--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -1,59 +1,43 @@
 import java.util.Arrays;
 
 public class Ticket implements Comparable<Ticket> {
-    public Ticket(int id, int price, String departure, String arrival, int time) {
-        this.id = id;
-        this.price = price;
-        this.departure = departure;
-        this.arrival = arrival;
-        this.time = time;
-        this.ticket1 = ticket1;
-    }
-
     protected int id;
     protected int price;
-    protected String departure;
-    protected String arrival;
+    protected String from;
+    protected String to;
     protected int time;
 
-    Ticket ticket1 = new Ticket(
-            10,
-            100,
-            "MOW",
-            "LED",
-            40
-    );
-    Ticket ticket2 = new Ticket(
-            11,
-            200,
-            "MOW",
-            "GOJ",
-            60
-    );
-    Ticket ticket3 = new Ticket(
-            13,
-            250,
-            "GOJ",
-            "LED",
-            90
-    );
-    Ticket ticket4 = new Ticket(
-            14,
-            180,
-            "MOW",
-            "FRU",
-            120
-    );
-    Ticket ticket5 = new Ticket(
-            15,
-            160,
-            "FRU",
-            "GOJ",
-            140
-    );
 
-    Ticket[] tickets = { ticket1, ticket2, ticket3, ticket4, ticket5 };
-    Arrays.sort(tickets);
+    public Ticket(int id, int price, String from, String to, int time) {
+        this.id = id;
+        this.price = price;
+        this.from = from;
+        this.to = to;
+        this.time = time;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+
 
     @Override
     public int compareTo(Ticket o) {
---
+++ src/test/java/TicketTest.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class TicketTest {
+
+
+    Ticket ticket1 = new Ticket(
+            12,
+            300,
+            "LED",
+            "GOJ",
+            90
+
+    );
+    Ticket ticket2 = new Ticket(
+            11,
+            200,
+            "MOW",
+            "GOJ",
+            60
+    );
+    Ticket ticket3 = new Ticket(
+            13,
+            250,
+            "GOJ",
+            "LED",
+            90
+    );
+    Ticket ticket4 = new Ticket(
+            14,
+            180,
+            "MOW",
+            "FRU",
+            120
+    );
+    Ticket ticket5 = new Ticket(
+            15,
+            160,
+            "FRU",
+            "GOJ",
+            140
+    );
+
+    Ticket[] tickets = { ticket1, ticket2, ticket3, ticket4, ticket5 };
+
+
+
+}
